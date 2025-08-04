9e14098977a3bd7a675113fd1a9232e7e64520c3
velonica <2120210430@mail.nankai.edu.cn>
2025-08-04 09:18:11 +0800

    my ttnn

---
+++ prepare_tt.sh
@@ -0,0 +1,6 @@
+export TT_METAL_HOME=$(pwd)
+export PYTHONPATH=$(pwd)
+export MESH_DEVICE=N300
+export WH_ARCH_YAML=wormhole_b0_80_arch_eth_dispatch.yaml
+export LD_LIBRARY_PATH=${TT_METAL_HOME}/build_Release/lib64/
+export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
