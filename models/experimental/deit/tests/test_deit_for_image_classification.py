a9f08b89a580d30abcdc85c8689feffbd06bf205
veloica0 <2120210430@mail.nankai.edu.cn>
2025-09-18 16:56:20 +0800

    Everything Done
    switch online model
    
    conflict
    
    cmake
    
    gitignore

--- models/experimental/deit/tests/test_deit_for_image_classification.py
+++ models/experimental/deit/tests/test_deit_for_image_classification.py
@@ -14,21 +14,16 @@ from models.utility_functions import (
     comp_allclose_and_pcc,
 )
 
-def test_deit_for_image_classification_inference(
-    device, hf_cat_image_sample_input, pcc=0.95
-):
+
+def test_deit_for_image_classification_inference(device, hf_cat_image_sample_input, pcc=0.95):
     with torch.no_grad():
         image = hf_cat_image_sample_input
 
         # real input
-        image_processor = AutoImageProcessor.from_pretrained(
-            "/home/openkylin/.cache/huggingface/hub/models--facebook--deit-base-distilled-patch16-224/snapshots/155831199e645cc8ec9ace65a38ff782be6217e1"
-        )
+        image_processor = AutoImageProcessor.from_pretrained("facebook/deit-base-distilled-patch16-224")
         inputs = image_processor(images=image, return_tensors="pt")
 
-        torch_model = DeiTForImageClassification.from_pretrained(
-            "/home/openkylin/.cache/huggingface/hub/models--facebook--deit-base-distilled-patch16-224/snapshots/155831199e645cc8ec9ace65a38ff782be6217e1"
-        )
+        torch_model = DeiTForImageClassification.from_pretrained("facebook/deit-base-distilled-patch16-224")
         torch_model.eval()
         state_dict = torch_model.state_dict()
         config = torch_model.config
